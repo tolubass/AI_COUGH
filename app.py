25e22b6fa4311fff22f10560cb0cbbd629108da2
Tolulope Olowolaju <tolajolowolaju81@gmail.com>
2025-07-31 23:32:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -213,3 +213,6 @@ def download_report(filename):
 # === Main ===
 if __name__ == '__main__':
     app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
